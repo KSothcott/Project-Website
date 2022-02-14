6ca97b246ccf2dd49ffd9a5cd94495e1207019d2
KSothcott <80410150+KSothcott@users.noreply.github.com>
2022-02-14 21:13:26 +0000

    Added header style, changed button font, added background colour

--- Website.php
+++ Website.php
@@ -1,24 +1,30 @@
 <html>
 <head>
 <style>
-a.button {
+	.header {
+		float: center;
+		font-size: 34px;
+		margin: 20px;
+		padding: 10px;
+		text-align: center;
+        font-family: sans-serif;
+	}
+    a.button {
         border: 2px solid black;
         background-color: white;
         padding: 10px;
         text-decoration: none;
         color: black;
+        font-family: sans-serif;
+        margin: 20px;
     }
 </style>
 </head>
-<body>
-<?php
-
-echo "Meal Planner and Shopping List";
-
-?>
-<br /><br />
-
-<a href="http://192.168.1.95/kathleen/shoppinglist.php" class="button">Click here to create a shopping list</a>
+<body style="background-color:#e6ffff;">
+	<div class="header">
+	<p>Meal Planning Website</p>
+	</div>
+    <a href="http://192.168.1.95/kathleen/shoppinglist.php" class="button">Click here to create a shopping list</a>
 
 
 </body>
