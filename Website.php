215a34655fae711e6c26f784abaa7ec0d83c7081
KSothcott <80410150+KSothcott@users.noreply.github.com>
2022-02-14 15:16:40 +0000

    Added link to shopping list from homepage

--- Website.php
+++ Website.php
@@ -2,8 +2,10 @@
 <body>
 <?php
 
-echo "GitHub Test";
+echo "Meal Planner and Shopping List";
 
 ?>
+<br /><br />
+<a href="http://192.168.1.95/kathleen/shoppinglist.php">Click here to create a shopping list</a>
 </body>
 </html>
