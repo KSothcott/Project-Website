8ea47cb8fda331209f5553928f2a6dde06758f45
KSothcott <80410150+KSothcott@users.noreply.github.com>
2022-02-15 15:04:41 +0000

    Added heading - 'Homepage'

--- Website.php
+++ Website.php
@@ -1,10 +1,10 @@
 <html>
 <head>
+<title>Homepage</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <style>
-	body {margin:0;}
-
+    
     .icon-bar {
     width: 100%;
     background-color: #7bc9c9;
@@ -28,6 +28,16 @@
     .active {
     background-color: #ffffff;
     }
+    
+    .heading {
+        float: center;
+		font-size: 34px;
+		margin: 20px;
+		padding: 10px;
+		text-align: center;
+        font-family: sans-serif;
+    }
+    
     a.button {
         border: 2px solid black;
         background-color: white;
@@ -40,19 +50,23 @@
 </style>
 </head>
 <body>
-<div class="icon-bar">
-<table style="width: 100%;">
-<tr>
-<td style="width 20%"><a href="http://192.168.1.95/kathleen/website.php"><i class="fa fa-home"></i></a></td>
-<td style="width 20%"></td>
-<td style="width 20%"></td>
-<td style="width 20%"></td>
-<td style="width 20%" ><a href="#"><i class="fa fa-gear"></i></a></td>
-</tr>
-</table>
-   
-   
-</div>
+    
+    <div class="icon-bar">
+    <table style="width: 100%;">
+    <tr>
+    <td style="width 20%"><a href="http://192.168.1.95/kathleen/website.php"><i class="fa fa-home"></i></a></td>
+    <td style="width 20%"></td>
+    <td style="width 20%"></td>
+    <td style="width 20%"></td>
+    <td style="width 20%" ><a href="#"><i class="fa fa-gear"></i></a></td>
+    </tr>
+    </table>
+    </div>
+    
+    <div class="heading">
+    <p>Homepage</p>
+    </div>
+    
     <br /><br />
     <a href="http://192.168.1.95/kathleen/shoppinglist.php" class="button">Click here to create a shopping list</a>
 
