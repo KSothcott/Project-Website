f477a7f5b483deeabb1ddad7a06e552f523b196d
KSothcott <80410150+KSothcott@users.noreply.github.com>
2022-02-15 15:13:24 +0000

    Added icon bar; increased text box size

--- shoppinglist.php
+++ shoppinglist.php
@@ -1,5 +1,7 @@
 <head>
 	<title>Shopping List</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<script>
 	function addItem() {
 		var newItem = document.createElement("div");
@@ -23,8 +25,32 @@
 	}
 	</script>
 	<style>
-	.header {
-		float: center;
+	.icon-bar {
+    width: 100%;
+    background-color: #7bc9c9;
+    overflow: auto;
+    }
+
+    .icon-bar a {
+    float: left;
+    width: 20%;
+    text-align: center;
+    padding: 12px 0;
+    transition: all 0.3s ease;
+    color: white;
+    font-size: 36px;
+    }
+
+    .icon-bar a:hover {
+    background-color: #94a0a1;
+    }
+
+    .active {
+    background-color: #ffffff;
+    }
+    
+    .heading {
+        float: center;
 		font-size: 34px;
 		margin: 20px;
 		padding: 10px;
@@ -54,20 +80,35 @@
     }
 	</style>
 </head>
-<body style="background-color:#e6ffff;">
-	<div class="header">
+<body>
+
+    <div class="icon-bar">
+    <table style="width: 100%;">
+    <tr>
+    <td style="width 20%"><a href="http://192.168.1.95/kathleen/website.php"><i class="fa fa-home"></i></a></td>
+    <td style="width 20%"></td>
+    <td style="width 20%"></td>
+    <td style="width 20%"></td>
+    <td style="width 20%" ><a href="#"><i class="fa fa-gear"></i></a></td>
+    </tr>
+    </table>
+    </div>
+
+	<div class="heading">
 	<p>Shopping List</p>
 	</div>
+    
 	<div class="app">
     <a href="http://192.168.1.95/kathleen/website.php" class="button">Click here to return home</a>
     <br /><br />
     <p>Type each item in the text box, then click Add Item <br/><br/> Click on an item in the list to remove it</p>
-	<input type="text" id="box"/>
+	<input type="text" id="box" size="70px"/>
 	<br/><br/>
 	<input type="button" value="Add item" onclick="addItem();"/>
 	<br/>
 	<div id="list"></div>
 	</div>
+    
 	<script>
 		if(localStorage.storedList) {
 		loadList();
