f7c178f97a86483cad5617ed36cc1906b3e1f235
KSothcott <80410150+KSothcott@users.noreply.github.com>
2022-02-22 15:46:13 +0000

    Added CSS to match the other pages

--- login.php
+++ login.php
@@ -3,8 +3,50 @@
 
     <title>User Login</title>
 
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+    
+    .icon-bar {
+    width: 100%;
+    background-color: #7bc9c9;
+    overflow: auto;
+    }
 
+    .icon-bar a {
+    float: left;
+    width: 20%;
+    text-align: center;
+    padding: 12px 0;
+    transition: all 0.3s ease;
+    color: white;
+    font-size: 40px;
+    text-decoration: none;
+    cursor: pointer;
+    }
 
+    .icon-bar a:hover {
+    background-color: #94a0a1;
+    }
+    
+    .heading {
+        float: center;
+		font-size: 34px;
+		margin: 20px;
+		text-align: center;
+        font-family: sans-serif;
+    }
+    
+    a.button {
+        border: 2px solid black;
+        background-color: white;
+        padding: 10px;
+        text-decoration: none;
+        color: black;
+        font-family: sans-serif;
+        margin: 20px;
+    }
+</style>
 </head>
 
 <body>
