b8eb6a2ae01c07f6b6160dadc7f5536a07548897
KSothcott <80410150+KSothcott@users.noreply.github.com>
2022-02-23 20:23:27 +0000

    Created page for a new user to register

---
+++ newuser.php
@@ -0,0 +1,66 @@
+<html>
+<head>
+<title>Homepage</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+    
+    .icon-bar {
+    width: 100%;
+    background-color: #7bc9c9;
+    overflow: auto;
+    }
+
+    .icon-bar a {
+    float: left;
+    width: 20%;
+    text-align: center;
+    padding: 12px 0;
+    transition: all 0.3s ease;
+    color: white;
+    font-size: 40px;
+    text-decoration: none;
+    cursor: pointer;
+    }
+
+    .icon-bar a:hover {
+    background-color: #94a0a1;
+    }
+
+    
+    .heading {
+        float: center;
+		font-size: 34px;
+		margin: 20px;
+		text-align: center;
+        font-family: sans-serif;
+    }
+    
+    a.button {
+        border: 2px solid black;
+        background-color: white;
+        padding: 10px;
+        text-decoration: none;
+        color: black;
+        font-family: sans-serif;
+        margin: 20px;
+    }
+</style>
+</head>
+
+<body>
+
+    <div class="icon-bar" style="font-family: sans-serif;">
+        <a href="http://192.168.1.95/kathleen/website.php" style="font-size: 47px;"><i class="fa fa-home"></i></a> 
+        <a style="background-color: #7bc9c9;">  </a> 
+        <a style="background-color: #7bc9c9;">My Meal Planner</a> 
+        <a style="background-color: #7bc9c9;">  </a>
+        <a href="#" style="font-size: 47px;"><i class="fa fa-gear"></i></a> 
+    </div>
+    
+    <div class="heading">
+    <p>Homepage</p>
+    </div>
+    
+</body>
+</html>
