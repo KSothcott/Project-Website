f3f931afa6259dab7299746c237db8ef503e018d
KSothcott <80410150+KSothcott@users.noreply.github.com>
2022-02-28 22:46:42 +0000

    New account info gets added to database when entered on website

--- newuserprocess.php
+++ newuserprocess.php
@@ -1,19 +1,22 @@
 
 <?php
-
-
+    include_once('header.php');
     $con = new PDO("mysql:host=127.0.0.1;dbname=userlogin","root","bi11andben");
     
-    $query = 'SELECT * FROM `userinfo` WHERE `Email` = "'.$_POST['email'].'"';
+    $query = 'SELECT * FROM `userinfo` WHERE `email`= "'.$_POST['email'].'"';
     
     $result = $con->query($query);
     $is_exist_email=sizeof($row = $result->fetch(PDO::FETCH_ASSOC))-1;
     
     if($is_exist_email) echo 'email exists';
-    else echo 'New email';
+
     
-    
+  $query = 'INSERT INTO `userlogin`.`userinfo` (`FirstName`, `LastName`, `Email`, `password`) VALUES ("'.$_POST['firstname'].'", "'.$_POST['lastname'].'", "'.$_POST['email'].'", ENCODE("'.$_POST['password'].'", "'.$keystring.'"))';   
+   
+   $con->query($query);   
     
     $con = null;
+
+    setcookie('cookievalue',$_POST['email'].time(),time()+(60*60));
 ?>
 
