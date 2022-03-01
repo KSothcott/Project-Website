5191c3abb3bf2ba89235ac66170f43596051cdc2
KSothcott <80410150+KSothcott@users.noreply.github.com>
2022-03-01 18:54:20 +0000

    Added cookie to header page
    Cookie checks the user is logged in when loading each page, and expires after one hour

--- header.php
+++ header.php
@@ -3,5 +3,16 @@
         date_default_timezone_set('EUROPE\LONDON');
         
         $keystring='dmmJHHnjj776bjjn';
+        $cookievalue = 'lsdkmcldsmclksdmcqwoeip';
+        
+        if(!strcmp($_COOKIE['cookievalue'],$cookievalue))
+        {
+            setcookie('cookievalue',$cookievalue,time()+3600);  
+        }
+        else
+        {
+            setcookie('cookievalue',"",0);
+            echo '<html><body onload="window.location.href='."'login.php'".'">></body></html>';                       
+        }
 
 ?>
