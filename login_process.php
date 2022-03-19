4dfc58d19d9d15d08b21277a93a228b0f5612007
KSothcott <80410150+KSothcott@users.noreply.github.com>
2022-03-19 15:48:50 +0000

    Create three cookies on login_process.php
    They are used to check the user is still logged in when a page is loaded, and expire after one hour of inactivity

--- login_process.php
+++ login_process.php
@@ -1,7 +1,6 @@
 <?php
 include_once('server.php');
  
-
 $query = 'SELECT `UserID`, `FirstName`, `LastName`, `Email`, DECODE(`password`, "'.$keystring.'") AS "PASSWORD"  FROM `userlogin`.`userinfo` WHERE `email`="'.$_POST['email'].'"';
 
 $result = $con->query($query);
@@ -11,12 +10,32 @@ $row = $result->fetch(PDO::FETCH_ASSOC);
 if(strcmp($_POST['email'],$row['Email'])) $checkemail = 0;
     else$checkemail = 1;
     
-if(strcmp($_POST['password'],$row['PASSWORD']))  $checkpassword=0;
+if(strcmp($_POST['password'],$row['PASSWORD']))  $checkpassword = 0;
    else $checkpassword = 1;
    
 if($checkemail && $checkpassword) 
 {
-    setcookie('cookievalue',$cookievalue,time()+3600); 
+    $query = "INSERT INTO `userlogin`.`logincookies`(`sessioncookie`,`time`,`UserID`) VALUES (UUID(),'".time()."', '".$row['UserID']."')";
+    
+    $con->query($query);
+    
+    $query = "SELECT * FROM `userlogin`.`logincookies` WHERE `UserID` = ".$row['UserID']." AND `time` = (SELECT MAX(`time`) FROM `userlogin`.`logincookies`)";
+    
+    $result = $con->query($query);
+    
+    $row = $result->fetch(PDO::FETCH_ASSOC);
+    
+    $userid = $row['UserID'];
+    
+    $sessionid = $row['SessionID'];
+    
+    $cookievalue = $row['sessioncookie'];
+    
+    setcookie('cookievalue',$cookievalue,$row['time']+3600); 
+    
+    setcookie('SessionID',$sessionid,$row['time']+3600);
+    
+    setcookie('UserID',$userid,$row['time']+3600); 
 }
 
 
