30850b72cd6d8e405099215259b094c46f9db7b1
KSothcott <80410150+KSothcott@users.noreply.github.com>
2022-03-19 15:45:04 +0000

    Fixed errors in cookiecheck.php
    The cookie was not being created correctly, and the login page continuously reloaded

--- cookiecheck.php
+++ cookiecheck.php
@@ -2,20 +2,19 @@
             date_default_timezone_set('Europe\London');
             include_once('server.php');
             
-            if(is_null($_COOKIE['user'])) echo '<body onload="window.location.href='."'login.php'".'">';
+            if(is_null($_COOKIE['cookievalue'])) echo '<body onload="window.location.href='."'login.php'".'">';
             else{
-            $query = 'SELECT * FROM `logincookies` WHERE `sessioncookie` = "'.$_COOKIE.'"';
+            $query = 'SELECT * FROM `logincookies` WHERE `sessioncookie` = "'.$_COOKIE['cookievalue'].'"';
             $result = $con->query($query);
-            if(!$row = $result->fetch(PDO::FETCH_ASSOC)) echo '<body onload="window.location.href='."'login.php'".'">';
+            if(!$row = $result->fetch(PDO::FETCH_ASSOC))  echo '<body onload="window.location.href='."'login.php'".'">';
             
-            if($row['TimeUpdate']+3600 < time()) echo '<body onload="window.location.href='."'login.php'".'">';
+            if($row['time']+3600 < time()) echo '<body onload="window.location.href='."'login.php'".'">';
             
-            $query = "UPDATE `userlogin`.`logincookies` SET `TimeUpdate` = '".time()."' WHERE `sessioncookie` = '".$_COOKIE['user']."'";
+            $query = "UPDATE `userlogin`.`logincookies` SET `time` = '".time()."' WHERE `SessionID` = '".$_COOKIE['SessionID']."'";
+            
             $con->query($query);
             
             }
 
 
 ?>
-
-<body onload="window.location.href='login.php'"></body>
