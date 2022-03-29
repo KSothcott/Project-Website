280d692767ef2425a34735f65244582655f7318f
KSothcott <80410150+KSothcott@users.noreply.github.com>
2022-03-29 14:03:36 +0100

    Added CSS to mealdetails.php and added button to return to meal plan

--- mealdetails.php
+++ mealdetails.php
@@ -51,7 +51,28 @@ function selectday($day,$meal){
         font-family: sans-serif;
     }
     
-    .button {
+    .button1 {
+        border: 2px solid black;
+        background-color: white;
+        padding: 10px;
+        text-decoration: none;
+        color: black;
+        font-family: sans-serif;
+        font-size: 15pt;
+        margin: 0px;
+        text-align: center;
+        position: absolute;
+        left: 40%;
+        right: 40%;
+    }
+    
+    .button1:hover {
+    background-color: #d1f1f1;
+    transition: all 0.4s ease;
+    box-shadow: 0 12px 16px 0 rgba(0,0,0,0.10), 0 17px 50px 0 rgba(0,0,0,0.10);
+    }
+    
+    .button2 {
         border: 2px solid black;
         background-color: white;
         padding: 10px;
@@ -61,12 +82,25 @@ function selectday($day,$meal){
         font-size: 15pt;
         margin: 50px;
         text-align: center;
+        float: right ;       
     }
     
-    .button:hover {
+    .button2:hover {
     background-color: #d1f1f1;
     transition: all 0.4s ease;
     box-shadow: 0 12px 16px 0 rgba(0,0,0,0.10), 0 17px 50px 0 rgba(0,0,0,0.10);
+    }
+    
+    .label {
+        margin: 20px;
+        font-family: sans-serif;
+        font-size: 17pt;
+    }
+    
+    .details {
+        margin: 20px;
+        font-family: sans-serif;
+        font-size: 14pt;
     }
     
 </style>
@@ -86,7 +120,7 @@ function selectday($day,$meal){
     </div>
     
    <?php 
-    $functionstring = '<button onclick ="selectday(';
+    $functionstring = '<button class="button1" onclick ="selectday(';
     $functionstring.= "'".$_GET['day']."', '";
     $functionstring.= $_GET['meal']."')";
     $functionstring.= '">Click here to edit this meal</button>';
@@ -99,11 +133,15 @@ function selectday($day,$meal){
     $query = 'SELECT '.$column_name.', '.$column_note.' FROM `mealplan` WHERE `UserID` = '.$_COOKIE['UserID'];
     $result = $con->query($query);
     $row = $result->fetch(PDO::FETCH_ASSOC);
-    echo 'Meal Name: '.$row[$column_name];
-    echo "<br /><br />";
-    echo 'Meal Notes: '.$row[$column_note];
-    
-?>
+    ?>
 
+    <a href="mealplanner.php" class="button2">Return to meal plan</a>
+    <br /><br />
+    <label class="label">Meal Name:</label>
+    <p class="details"><?php echo "$row[$column_name]"?></p>
+    <br /><br />
+    <label class="label">Meal Notes:</label>
+    <p class="details"><?php echo "$row[$column_note]"?></p>
+    
 </body>
 </html>
